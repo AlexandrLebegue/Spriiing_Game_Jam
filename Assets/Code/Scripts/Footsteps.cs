46cca7fb1fa9392b7f27071b00c928fcde52f5c2
Orée <83070309+Enoryel@users.noreply.github.com>
2023-03-25 19:14:39 +0100

    Footsteps Update
    - import fichiers audio
    - ajout bruits de pas
    - ajouts son environnement monde de l'éveil

---
+++ Assets/Code/Scripts/Footsteps.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public AudioSource footstepsSound;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Q) & Input.GetKey(KeyCode.D))
+        {
+            footstepsSound.enabled = false; 
+        }
+        else if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.D))
+        {
+            footstepsSound.enabled = true;
+        }
+        else
+        {
+            footstepsSound.enabled = false;
+        }
+    }
+}
