95fa41f7f8e17a378ec15b9b4b49c3a4d346eb59
Orée <83070309+Enoryel@users.noreply.github.com>
2023-03-26 00:41:50 +0100

    Merge w/ Main + sounds

--- Assets/Code/Scripts/Footsteps.cs
+++ Assets/Code/Scripts/Footsteps.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class Footsteps : MonoBehaviour
 {
     public AudioSource footstepsSound;
+    public Animator animator;
+
+    
 
     private void Update()
     {
-        if ((Input.GetKey(KeyCode.Q) & Input.GetKey(KeyCode.D)) || (Input.GetKey(KeyCode.LeftArrow) & Input.GetKey(KeyCode.RightArrow)))
-        {
-            footstepsSound.enabled = false; 
+        if (animator.GetBool("Is_Walking")){
+            if ((Input.GetKey(KeyCode.Q) & Input.GetKey(KeyCode.D)) || (Input.GetKey(KeyCode.LeftArrow) & Input.GetKey(KeyCode.RightArrow)))
+            {
+                footstepsSound.enabled = false;
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.D))
+            {
+                footstepsSound.enabled = true;
+            }
+            else
+            {
+                footstepsSound.enabled = false;
+            }
         }
-        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.D))
-        {
-            footstepsSound.enabled = true;
-        }
-        else
-        {
-            footstepsSound.enabled = false;
-        }
+        
     }
 }
